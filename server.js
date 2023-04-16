7d131417c34a1061b6e1a3df4074a94d5f0f0820
hung0920 <hungooso4413@gmail.com>
2023-04-16 13:58:58 +0700

    Add package

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Model from './src/app/model/NhanVienModel'
+import {engine} from 'express-handlebars';
+
+const port = 8888;
+const db = 'mongodb';
+const app = express();
+
+app.use(express.urlencoded({
+    extended: true,
+}))
+app.use(express.json());
+
+app.engine(".hbs", engine({
+    extname: ".hbs",
+}))
+app.set("view engine", ".hbs");
+app.set("views", "./src/views")
+
+app.get("/", async (req, res) => {
+    res.render("home");
+})
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+})
