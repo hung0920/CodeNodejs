72f7931ec4e6b34f2c19d985aad5915b6af4dc1e
hung0920 <hungooso4413@gmail.com>
2023-04-17 17:28:33 +0700

    Clone code

--- server.js
+++ server.js
@@ -1,21 +1,51 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import {engine} from 'express-handlebars';
+import { engine } from 'express-handlebars';
+import multer from 'multer';
 
 const port = 8888;
-const db = 'mongodb';
+const db = "mongodb://127.0.0.1:27017/MOB402";
 const app = express();
 
-app.use(express.urlencoded({
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./src/public/src");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+var upload = multer({ storage: storage });
+
+app.use(express.static("./src/public"));
+
+app.use(
+  express.urlencoded({
     extended: true,
-}))
+  })
+);
 app.use(express.json());
 
-app.engine(".hbs", engine({
+app.engine(
+  ".hbs",
+  engine({
     extname: ".hbs",
-}))
+  })
+);
 app.set("view engine", ".hbs");
-app.set("views", "./src/views")
+app.set("views", "./src/views");
+
+async function connectdb() {
+  try {
+    await mongoose.connect(db);
+    console.log("OK");
+  } catch (error) {
+    console.log("FAIL");
+  }
+}
+
+connectdb();
 
 app.get("/", async (req, res) => {
     res.render("home");
